eae820504dc5be759b4375b144588e373604d610
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-07-06 15:32:40 -0300

    sube versiones

--- src/server/server-procesamiento.ts
+++ src/server/server-procesamiento.ts
@@ -1,12 +1,9 @@
 "use strict";
 
 import {emergeAppProcesamiento} from "./app-procesamiento"
-import {emergeAppOperativos, AppBackend} from "operativos"
-import {emergeAppDatosExt} from "datos-ext"
+import {emergeAppDatosExt, emergeAppOperativos, AppBackend} from "datos-ext"
 import {emergeAppVarCal} from "varcal"
 
-// var AppProcesamiento = emergeAppProcesamiento(emergeAppVarCal(emergeAppOperativos(AppBackend)));
-// TODO AGREGAR A HERENCIA DATOS EXT asi:
 var AppProcesamiento = emergeAppProcesamiento(emergeAppVarCal(emergeAppDatosExt(emergeAppOperativos(AppBackend))));
 
 new AppProcesamiento().start();
