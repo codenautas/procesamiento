ebd25e28b50574d7245b0681c27cf6c56706b724
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-07-17 11:21:13 -0300

    refactor y encapsulamiento de manejo de procedures desde app

--- src/server/app-procesamiento.ts
+++ src/server/app-procesamiento.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 import * as varcal from "varcal";
-import {ProceduresProcesamiento} from "./procedures-procesamiento";
+import {procedures} from "./procedures-procesamiento";
 
 export type Constructor<T> = new(...args: any[]) => T;
 export function emergeAppProcesamiento<T extends Constructor<varcal.VarCalType>>(Base:T){
@@ -9,22 +9,9 @@ export function emergeAppProcesamiento<T extends Constructor<varcal.VarCalType>>
     return class AppProcesamiento extends Base{
         constructor(...args:any[]){ 
             super(...args);
+            this.myProcedures = this.myProcedures.concat(procedures);
         }
-        getProcedures(){
-            //TODO: es igual que en datos-ext llevarlo a varcal
-            var be = this;
-            return super.getProcedures().then(function(procedures){
-                return procedures.concat(
-                    ProceduresProcesamiento.map(be.procedureDefCompleter, be)
-                );
-            });
-        }    
-        // clientIncludes(req:varcal.Request, hideBEPlusInclusions:boolean){
-        //     //TODO: es igual que en datos-ext llevarlo a varcal
-        //     return super.clientIncludes(req, hideBEPlusInclusions).concat([
-        //         {type:'js' , src:'client/procesamiento.js'},
-        //     ])
-        // }
+
         getMenu():varcal.MenuDefinition{
             //TODO: es igual que en datos-ext llevarlo a varcal
             let myMenuPart:varcal.MenuInfo[]=[
--- src/server/procedures-procesamiento.ts
+++ src/server/procedures-procesamiento.ts
@@ -1,18 +1,4 @@
 "use strict";
-import * as Varcal from "varcal";
-// import * from "varcal";
-
-var ProceduresProcesamiento = [
-    {
-        action: 'saludos/hola',
-        parameters: [
-            {name:'operativo'     ,references:'operativos',  typeName:'text'},
-        ],
-        coreFunction: async function(context:Varcal.ProcedureContext, parameters: Varcal.coreFunctionParameters){
-            var sqlParams=[parameters.varcal] + context.username;
-            return sqlParams;
-        }
-    },
-];
-
-export {ProceduresProcesamiento};
+import { ProcedureDef } from "varcal";
+var procedures: ProcedureDef[] = [];
+export {procedures};
