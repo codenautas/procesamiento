8cc89610b54d2ccaf82cc915a91706230139992f
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-09-07 10:44:03 -0300

    torta

--- src/client/procesamiento.ts
+++ src/client/procesamiento.ts
@@ -1,1 +1,6 @@
 "use strict";
+//# sourceMappingURL=procesamiento.js.map
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    document.body.style='background-repeat: no-repeat; background-image: url("img/torta.png")'
+});
