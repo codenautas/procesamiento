0b61aa2c32654c5bbcffe9f1d03bf6c249d50352
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-09-11 11:32:33 -0300

    borra torta

--- src/client/procesamiento.ts
+++ src/client/procesamiento.ts
@@ -1,6 +1,2 @@
 "use strict";
 //# sourceMappingURL=procesamiento.js.map
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-    document.body.style='background-repeat: no-repeat; background-image: url("img/torta.png"); background-position: top'
-});
