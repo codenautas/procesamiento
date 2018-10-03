12f127b18775ad3dac6f5f47caf1737e9a3f5f0e
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-10-03 15:37:51 -0300

    usa paquete de capa de app de backend-plus "consistencias"

--- src/server/server-procesamiento.ts
+++ src/server/server-procesamiento.ts
@@ -1,10 +1,11 @@
 "use strict";
 
 import { AppBackend, emergeAppDatosExt, emergeAppOperativos } from "datos-ext";
+import { emergeAppConsistencias } from "consistencias";
 import { emergeAppExportador } from "exportador";
 import { emergeAppVarCal } from "varcal";
 import { emergeAppProcesamiento } from "./app-procesamiento";
 
-var AppProcesamiento = emergeAppProcesamiento(emergeAppVarCal(emergeAppExportador(emergeAppDatosExt(emergeAppOperativos(AppBackend)))));
+var AppProcesamiento = emergeAppProcesamiento(emergeAppVarCal(emergeAppConsistencias(emergeAppExportador(emergeAppDatosExt(emergeAppOperativos(AppBackend))))));
 
 new AppProcesamiento().start();
