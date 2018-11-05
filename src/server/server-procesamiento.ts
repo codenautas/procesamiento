742d66a357d8a307ecb2d20c28898a29be00e99e
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-11-05 13:10:18 -0300

    saco modulo exportador de bases de usuario

--- src/server/server-procesamiento.ts
+++ src/server/server-procesamiento.ts
@@ -2,10 +2,10 @@
 
 import { AppBackend, emergeAppDatosExt, emergeAppOperativos } from "datos-ext";
 import { emergeAppConsistencias } from "consistencias";
-import { emergeAppExportador } from "exportador";
+// import { emergeAppExportador } from "exportador";
 import { emergeAppVarCal } from "varcal";
 import { emergeAppProcesamiento } from "./app-procesamiento";
 
-var AppProcesamiento = emergeAppProcesamiento(emergeAppVarCal(emergeAppConsistencias(emergeAppExportador(emergeAppDatosExt(emergeAppOperativos(AppBackend))))));
+var AppProcesamiento = emergeAppProcesamiento(emergeAppVarCal(emergeAppConsistencias(emergeAppDatosExt(emergeAppOperativos(AppBackend)))));
 
 new AppProcesamiento().start();
